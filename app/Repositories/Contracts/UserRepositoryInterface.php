eec1d4e06760a67ec06d6d4294cee01d32bf0c0c
Господин <fiesto73@icloud.com>
2026-03-25 12:49:53 +0400

    - Дозагрузка интерфейса

---
+++ app/Repositories/Contracts/UserRepositoryInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repositories\Contracts;
+
+use App\Models\BotUsers;
+use App\Models\TempAdvUser;
+
+interface UserRepositoryInterface
+{
+    public function findByChatId(int $chatId): ?BotUsers;
+
+    public function UpdateUser(int $chatId, array $data): void;
+
+    public function updateTempAdv(int $userId, array $data): void;
+
+    public function getAdvRow(int $chatId): ?TempAdvUser;
+}
