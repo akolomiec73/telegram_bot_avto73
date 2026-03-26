147388a8d47c501c6fa28534dc6ccc772dd9c0d8
Господин <fiesto73@icloud.com>
2026-03-26 14:50:34 +0400

    -Добавлены новые стадии для "Запчасти" и переименованы стадии для "Транспорт"

--- app/Constant/UserStages.php
+++ app/Constant/UserStages.php
@@ -6,16 +6,23 @@ namespace App\Constant;
 
 class UserStages
 {
-    // Создание объявления
-    public const POST_ADV_STEP1 = 'post_set_category_step1';
+    // Создание объявления Транспорт
+    public const POST_ADV_STEP1 = 'post_set_title_step1';
 
-    public const POST_ADV_STEP2 = 'post_set_title_step2';
+    public const POST_ADV_STEP2 = 'post_set_caryear_step2';
 
-    public const POST_ADV_STEP3 = 'post_set_caryear_step3';
+    public const POST_ADV_STEP3 = 'post_set_price_step3';
 
-    public const POST_ADV_STEP4 = 'post_set_price_step4';
+    public const POST_ADV_STEP4 = 'post_set_description_step4';
 
-    public const POST_ADV_STEP5 = 'post_set_description_step5';
+    public const POST_ADV_STEP5 = 'post_set_photo_step5';
 
     public const POST_ADV_STEP6 = 'post_set_contact_step6';
+
+    // Создание объявления Запчасти
+    public const POST_ADV_DETAIL_STEP1 = 'post_set_title_detail_step1';
+
+    public const POST_ADV_DETAIL_STEP2 = 'post_set_price_detail_step2';
+
+    public const POST_ADV_DETAIL_STEP3 = 'post_set_description_detail_step3';
 }
