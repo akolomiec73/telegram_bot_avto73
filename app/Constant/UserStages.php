dfa908cfa11fc01c63b2e234b5e7a9a9454bdf91
Господин <fiesto73@icloud.com>
2026-03-27 11:53:57 +0400

    - Изменены константы для новой логики обработки

--- app/Constant/UserStages.php
+++ app/Constant/UserStages.php
@@ -7,24 +7,24 @@ namespace App\Constant;
 class UserStages
 {
     // Создание объявления Транспорт
-    public const POST_ADV_STEP1 = 'post_set_title_step1';
+    public const POST_ADV_STEP1 = 'post_set_category_step1';
 
-    public const POST_ADV_STEP2 = 'post_set_caryear_step2';
+    public const POST_ADV_STEP2 = 'post_set_title_step2';
 
-    public const POST_ADV_STEP3 = 'post_set_price_step3';
+    public const POST_ADV_STEP3 = 'post_set_caryear_step3';
 
-    public const POST_ADV_STEP4 = 'post_set_description_step4';
+    public const POST_ADV_STEP4 = 'post_set_price_step4';
 
-    public const POST_ADV_STEP5 = 'post_set_photo_step5';
+    public const POST_ADV_STEP5 = 'post_set_description_step5';
 
-    public const POST_ADV_STEP6 = 'post_set_contact_step6';
+    public const POST_ADV_STEP6 = 'post_set_photo_step6';
+
+    public const POST_ADV_STEP7 = 'post_set_contact_step7';
 
     // Создание объявления Запчасти
-    public const POST_ADV_DETAIL_STEP1 = 'post_set_title_detail_step1';
+    public const POST_ADV_DETAIL_STEP1 = 'post_set_category_detail_step1';
 
-    public const POST_ADV_DETAIL_STEP2 = 'post_set_price_detail_step2';
-
-    public const POST_ADV_DETAIL_STEP3 = 'post_set_description_detail_step3';
+    public const POST_ADV_DETAIL_STEP2 = 'post_set_title_detail_step2';
 
     // callback для кнопок
     public const BUTTON_BACK_MAIN_MENU = 'back_main_menu';
